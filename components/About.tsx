bd6593f69b2e586765f160eae7dbfb1a90c06c0b
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-02-18 11:49:24 +0100

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -19,4 +19,16 @@ const About = () => {
 
       {/* Text Section */}
       <div className="max-lg:w-full w-[50%] mx-auto my-10">
-        <div className="max-md:text-3
+        <div className="max-md:text-3xl text-5xl font-semibold title after:w-24 after:bg-emerald-800 after:h-2 after:flex">
+          <span className="text-emerald-800">About </span>Us
+        </div>
+        <div className="max-lg:max-w-[100%] max-w-[70%] w-full my-5">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit neque rem, eum sequi cumque totam dolor inventore incidunt quo nihil, tenetur optio facere quisquam! Assumenda iure alias dolor quo aut. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi adipisci esse dolorem nostrum rem fugit autem architecto, sint tempora aspernatur distinctio eos quos suscipit exercitationem alias, nobis placeat, accusamus error!
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default About;
