7cc3a22b4453a45810365457d2433b8fdeb8783c
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-02-17 23:53:32 -0800

    Update Discount.tsx

--- components/Discount.tsx
+++ components/Discount.tsx
@@ -4,7 +4,12 @@ import Button from './Button';
 const Discount = () => {
   return (
     <div className='w-full min-h-[200px] max-md:max-h-[300px] h-full max-h-[400px] text-center flex flex-col items-center relative' >
-        <div className='w-full' ><img className='w-full object-cover  min-h-[200px] ' src="/discount.png" alt="discount" /></div>
+        <div className='w-full' >  <Image
+      src="/discount.png"
+      width={''}
+      height={''}
+      className='w-full object-cover  min-h-[200px] '
+      alt="discount"/></div>
         <div className='absolute left-[5%] max-lg:top-[10%] top-[20%] ' >
       <div className='max-lg:text-4xl text-[60px]' >Discount <span className='text-white' >Sale</span></div>
       <div className='max-lg:text-5xl text-[80px] font-semibold' ><span className='text-emerald-500' >30%</span>Off</div>
