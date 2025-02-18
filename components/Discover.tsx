ceea5d4cfcde9499f202a21aef37e56c5675eb8c
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-02-18 09:30:48 +0100

    Update Discover.tsx

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -14,7 +14,7 @@ const Discover = () => {
       alt="discover"/>
         <div className="absolute top-[30%] left-[10%] flex flex-col items-center  ">
             <div className='max-md:text-3xl max-xl:text-5xl text-6xl w-[70%] text-center  ' ><span className='text-white' >Discover</span> Your <span className='text-white' >latest</span> collection on <span className='text-white' >our</span> shop</div>
-            <div className='my-10 max-md:scale-90 ' > <Button paddingX='max-md:px-5 px-20'  text='Explore Now' icon={<BiArrowBack className='rotate-[180deg]' />} /></div>
+            <div className='my-10 max-md:scale-90 ' > <Button paddingX='max-md:px-5 px-20' link="https://wa.link/l2w6s6" text='Explore Now' icon={<BiArrowBack className='rotate-[180deg]' />} /></div>
         </div>
     </div>
   )
