c12221a70c21085bd00843bef67238c43c47a1be
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-02-18 09:31:54 +0100

    Update Collection.tsx

--- components/Collection.tsx
+++ components/Collection.tsx
@@ -8,7 +8,7 @@ const Collection = () => {
     <div id='collection' >
         <div className='flex flex-row max-md:flex-col gap-y-5 justify-between max-md:mx-5 mx-20 my-20' >
             <div className=' max-md:text-3xl max-xl:text-4xl text-5xl max-md:w-full w-[50%] '><span className='text-emerald-800' >Upgrade</span> Your <span className='text-emerald-800' >Style</span> with Our Weekly <span className='text-emerald-800' >Collection</span></div>
-            <Button text='Shop now' paddingX='p-10'  icon={<BiArrowBack className='rotate-[180deg]' />} />
+            <Button text='Shop now' link="https://wa.link/l2w6s6" paddingX='p-10'  icon={<BiArrowBack className='rotate-[180deg]' />} />
         </div>
         <div className="grid max-lg:grid-cols-1 grid-cols-2 gap-10  max-md:mx-5 mx-20 mb-20 ">
         <div className='w-full relative ' > <Image
