595f2bbbd2b700f9a21ba97fef0ada934868fd97
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-02-22 01:22:23 +0100

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -11,7 +11,7 @@ const Contact = () => {
   const [firstName, setFirstName] = useState<string>();
   const [lastName, setLastName] = useState<string>();
   const [email, setEmail] = useState<string>();
-  const [phone, setPhone] = useState<number>();
+  const [phone, setPhone] = useState<string>();
   const [message, setMessage] = useState<string>();
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>();
