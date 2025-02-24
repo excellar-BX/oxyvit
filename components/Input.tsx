1fbc952bc68565eefc8d79a5923221a5b1d4847d
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-02-24 13:13:49 +0100

    Update Input.tsx

--- components/Input.tsx
+++ components/Input.tsx
@@ -7,7 +7,7 @@ type InputProps = {
     placeholder:string,
     required?: boolean,
     value?: string,
-    onChange?: (e: React.ChangeEvent<HTMLInputElement>),
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void,
 }
 
 const Input = ({label,type, icon, placeholder, required, value, onChange}:InputProps) => {
